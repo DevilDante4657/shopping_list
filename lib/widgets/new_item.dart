0876eed13d0c9996efcbb79dbfd32e1132da09b6
DevilDante4657 <kingdante4657@gmail.com>
2025-11-25 11:56:46 -0500

    made initial formfields

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -14,7 +16,55 @@ class _NewItemState extends State<NewItem>{
         title: Text("Add a new item..."),
       ),
       body: Padding(padding: EdgeInsets.all(12),
-      child: Text("Form Goes Here"),
+      child: Form(
+        child:Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: InputDecoration(
+                label: Text("Name"),
+              ),
+              validator: (value){
+                return null;
+              },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    label: Text("Quantity")
+                  ),
+                  initialValue: '1',
+                ),
+              ),
+              const SizedBox(width: 5),
+              Expanded(
+                child: DropdownButtonFormField(
+                  items: [
+                    for(final category in categories.entries)
+                    DropdownMenuItem(
+                      value: category.value,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 16,
+                            height: 16,
+                            color: category.value.color,
+                          ),
+                          const SizedBox(width: 6),
+                          Text(category.value.title)
+                        ],
+                      ))  
+                  ], 
+                  onChanged: (value){}
+                  ),
+              )
+            ],)
+          ],
+        ) 
+        ),
       ),
     );
   }
